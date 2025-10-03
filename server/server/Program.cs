5e953998fe336653429dd266b98d857c3eacf594
lipe-s-m <felipe.s.m1609@gmail.com>
2025-10-03 00:24:21 -0300

    :sparkles: Primeiras Rotas

--- server/server/Program.cs
+++ server/server/Program.cs
@@ -1,8 +1,45 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Run();
 
 app.MapGet("/", () => "Hello World!");
+app.MapPost("/login", (LoginRequestDTO loginRequestDTO) =>
+{
+	if (loginRequestDTO.Email != null && loginRequestDTO.Senha != null)
+	{
+		return Results.Ok("Usuario Logado com Sucesso!");
+	}
+
+	return Results.Unauthorized();
+
+});
+
+app.MapPost("/cadastro", (CadastroRequestDTO cadastroRequestDTO) =>
+{
+	if (cadastroRequestDTO.Nome != null && cadastroRequestDTO.Senha != null)
+	{
+		return Results.Ok("Usuario criado com sucesso");
+	}
+	return Results.BadRequest("Erro! Campos inválidos!");
+});
+
+app.Run();
 
 
+
+
+
+//classes
+class LoginRequestDTO
+{
+	public string Email { get; set; }
+	public string Senha { get; set; }
+}
+
+class CadastroRequestDTO
+{
+	public string Nome { get; set; }
+	public string Email { get; set; }
+	public string Senha { get; set; }
+	public string SenhaConfirmacao { get; set; }
+}
