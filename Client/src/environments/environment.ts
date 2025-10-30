00b7375b335f4e772b2dd7131f6ebf19f2f77172
lipe-s-m <felipe.s.m1609@gmail.com>
2025-10-30 18:37:02 -0300

    :sparkles: Pagina de Perfil v1

--- Client/src/environments/environment.ts
+++ Client/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   apiUrlProd:
     'https://aprendanaveserver-fbegdjfeb6hpb8an.brazilsouth-01.azurewebsites.net',
-  apiUrlDev: 'http://localhost:5269',
-  // apiUrlDev:
-  // 'https://aprendanaveserver-fbegdjfeb6hpb8an.brazilsouth-01.azurewebsites.net',
+  // apiUrlDev: 'http://localhost:5269',
+  apiUrlDev:
+    'https://aprendanaveserver-fbegdjfeb6hpb8an.brazilsouth-01.azurewebsites.net',
 };
